5b5ebbf6c03f5e5d2961bd37b1521ad4d6f5842e
jaslowdigital <116821692+jaslowdigital@users.noreply.github.com>
2025-04-27 17:52:32 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// main.js - Custom JavaScript for The Best Phone Plans
+
+// Currently empty
+// This file is prepared for future JavaScript functionality
+
+// Example: Smooth scroll to top, form validations, dynamic features can be added here
+
+console.log("The Best Phone Plans website loaded successfully.");
