d478ec7d78617590dc1ba9a086cb77e9b5dcdd15
jaslowdigital <116821692+jaslowdigital@users.noreply.github.com>
2025-04-27 19:04:04 -0400

    Delete js directory

--- js/main.js
+++
@@ -1,8 +0,0 @@
-// main.js - Custom JavaScript for The Best Phone Plans
-
-// Currently empty
-// This file is prepared for future JavaScript functionality
-
-// Example: Smooth scroll to top, form validations, dynamic features can be added here
-
-console.log("The Best Phone Plans website loaded successfully.");
